9e8267c22608b1abdc34369c828f6e1cce1477ab
TridevSharma1 <tridevx8@gmail.com>
2026-04-26 17:15:16 +0530

    URL

--- CRUD_operation/student_crud/urls.py
+++ CRUD_operation/student_crud/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+from .views import * 
 
 urlpatterns = [
-    path('students', views.student_list, name='student_list'),
-    path('add/', views.add_student),
-    # path('delete/<int:id>/', views.delete_student),
-    # path('edit/<int:id>/', views.edit_student),
+    path('students', student_list, name='student_list'), 
+    path('add/', add_student, name='add_student'),
+    path('delete/<int:id>/', delete_student, name='delete_student'),
+    path('edit/<int:id>/', edit_student, name='edit_student'),
 ]
